5248826b75e6a3e7b84f2f401d266b4fd2a0e62f
James Anderson <James137137@users.noreply.github.com>
2021-07-23 19:21:39 +1200

    Update SetRep.java

--- src/main/java/commands/SetRep.java
+++ src/main/java/commands/SetRep.java
@@ -31,7 +31,7 @@ public class SetRep {
         Role role = event.getGuild().getRolesByName(Settings.HardClearName, false).get(0);
         List<Member> mentionedMembers = event.getMessage().getMentionedMembers();
         if (mentionedMembers.isEmpty()) {
-            return "You forgot to tag someone, silly!";
+            return "You forgot to tag someone, silly e.g. <@ " + event.getAuthor().getId() + ">!";
         }
 
         if (mentionedMembers.size() > 1) {
