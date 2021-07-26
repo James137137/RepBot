39df6ee5cc92963ae5512beae279751e83c4aa27
James Anderson <James137137@users.noreply.github.com>
2021-07-26 21:07:19 +1200

    so we don't fuck up

--- src/main/java/com/javadog/repbot/Main.java
+++ src/main/java/com/javadog/repbot/Main.java
@@ -3,6 +3,7 @@ package com.javadog.repbot;
 import event.OnChat;
 import event.listener;
 import java.io.IOException;
+import java.util.Scanner;
 import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -20,7 +21,14 @@ public class Main {
     
     public static JDA jda;
     public static void main(String[] args) throws LoginException, InterruptedException {
-        
+        System.out.println("DEBUG MODE IS ENABLE....PANIC!!!!!!!!!!");
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("type jack is 2nd kiwi to start");
+        String nextLine = myScanner.nextLine();
+        if (!nextLine.equalsIgnoreCase("jack is 2nd kiwi"))
+        {
+            System.exit(0);
+        }
         
         Settings.startup();
         try {
