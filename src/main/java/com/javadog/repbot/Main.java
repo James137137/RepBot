d3a65643be6eb3b3377ee5846d7d65b87091aab6
James Anderson <James137137@users.noreply.github.com>
2021-07-26 21:08:05 +1200

    so we don't fuck up with debug

--- src/main/java/com/javadog/repbot/Main.java
+++ src/main/java/com/javadog/repbot/Main.java
@@ -8,56 +8,49 @@ import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
-
-
 /**
  *
  * @author James Anderson
  */
 public class Main {
-    
-    
+
     public static boolean debug = false;
-    
+
     public static JDA jda;
+
     public static void main(String[] args) throws LoginException, InterruptedException {
-        System.out.println("DEBUG MODE IS ENABLE....PANIC!!!!!!!!!!");
-        Scanner myScanner = new Scanner(System.in);
-        System.out.println("type jack is 2nd kiwi to start");
-        String nextLine = myScanner.nextLine();
-        if (!nextLine.equalsIgnoreCase("jack is 2nd kiwi"))
-        {
-            System.exit(0);
+        if (debug) {
+            System.out.println("DEBUG MODE IS ENABLE....PANIC!!!!!!!!!!");
+            Scanner myScanner = new Scanner(System.in);
+            System.out.println("type jack is 2nd kiwi to start");
+            String nextLine = myScanner.nextLine();
+            if (!nextLine.equalsIgnoreCase("jack is 2nd kiwi")) {
+                System.exit(0);
+            }
         }
-        
+
         Settings.startup();
         try {
             VoteHistoryDataBase.load();
         } catch (IOException ex) {
             VoteHistoryDataBase.saveFirstTime();
         }
-        
+
         boolean loaded;
         try {
             UserRepDataBase.load();
         } catch (Exception ex) {
             UserRepDataBase.save();
         }
-        
+
         jda = JDABuilder.createDefault(Settings.TOKEN)
-            .addEventListeners(new listener())
-            .build();
+                .addEventListeners(new listener())
+                .build();
 
         // optionally block until JDA is ready
         jda.awaitReady();
         jda.addEventListener(new OnChat());
-        
-        
-        
-        
-        
-        
+
     }
 
-    
 }
